8367fb16c4b130b34d283fc4da4b2e18db2d9f87
DavidYS0004 <dsas0004@student.monash.edu>
2023-08-18 16:35:51 +1000

    motor_running

--- RPI/testing.py
+++ RPI/testing.py
@@ -8,29 +8,37 @@ import gpiozero
 import multiprocessing
 from multiprocessing import Value
 
-EN_port = gpiozero.PWMOutputDevice(pin=12,active_high=True,initial_value=0,frequency=100) #RAEDM GPIO12
-#ENB_port = gpiozero.OutputDevice(pin=4)
+LEFT_EN_port = gpiozero.PWMOutputDevice(pin=21,active_high=True,initial_value=0,frequency=100) #RAEDM GPIO12
+RIGHT_EN_port = gpiozero.PWMOutputDevice(pin=20,active_high=True,initial_value=0,frequency=100) #RAEDM GPIO12
 
-LEFT_PWM1_port = gpiozero.DigitalOutputDevice(pin=17, active_high = True,initial_value = True)
-LEFT_PWM2_port = gpiozero.DigitalOutputDevice(pin=27, active_high = True,initial_value = True)
 
-encoder = gpiozero.RotaryEncoder(a=21, b=20,max_steps=100000) 
+LEFT_PWM1_port = gpiozero.DigitalOutputDevice(pin=19, active_high = True,initial_value = True)
+LEFT_PWM2_port = gpiozero.DigitalOutputDevice(pin=26, active_high = True,initial_value = True)
+
+RIGHT_PWM1_port = gpiozero.DigitalOutputDevice(pin=27, active_high = True,initial_value = True)
+RIGHT_PWM2_port = gpiozero.DigitalOutputDevice(pin=1----------------------------------------------------------------------7, active_high = True,initial_value = True)
+
+#encoder = gpiozero.RotaryEncoder(a=21, b=20,max_steps=100000) 
 # Step through duty cycle values, slowly increasing the speed and changing the direction of motion
 
-encoder.steps = 0
+#encoder.steps = 0
 for j in range(10):
-    EN_port.value = j/10
-    #ENB_port.off() #sets to 0
+    LEFT_EN_port.value = j/10
+    RIGHT_EN_port.value = j/10
+    #ENB_port.off() #sets to 0, physically connected to ground
     
-    #logic PWM1 PWM2 1 0
-    PWM1_port.on()                                                                                                   
-    PWM2_port.off()
+    #FORWARD
+    #LEFT WHEEL
+    LEFT_PWM1_port.on()                                                                                                   
+    LEFT_PWM2_port.off()
 
-    print('Duty cycle:',EN_port.value,'Direction:',PWM1_port.value, PWM2_port.value)
+    #RIGHT WHEEL
+    RIGHT_PWM1_port.on()                                                                                                   
+    RIGHT_PWM2_port.off()
+
+    print('Duty cycle:',LEFT_EN_port.value,'Direction:',LEFT_PWM1_port.value, LEFT_PWM2_port.value)
     time.sleep(5.0)
-    print('Counter:',encoder.steps,'Speed:',(encoder.steps)/5.0,'steps per second\n')
-    encoder.steps = 0
-
-EN_port.value =0 
+  #  print('Counter:',encoder.steps,'Speed:',(encoder.steps)/5.0,'steps per second\n')
+ #   encoder.steps = 0
 
 
